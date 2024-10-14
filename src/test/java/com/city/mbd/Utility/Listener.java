f78a1ac694a608230d56858dea69abde3ca9b069
Avdhut Bhise <avdhut.bhise@gmail.com>
2024-10-14 15:15:17 +0530

    Adding modified Files

--- src/test/java/com/city/mbd/Utility/Listener.java
+++ src/test/java/com/city/mbd/Utility/Listener.java
@@ -1,5 +1,6 @@
 package com.city.mbd.Utility;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -17,6 +18,30 @@ public class Listener extends BaseClass implements ITestListener{
 	}
 	
 	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
 		
 	}
